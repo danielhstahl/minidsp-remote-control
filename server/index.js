7db6b68c052aefa5f54a70f30913cfa000815495
Daniel Stahl <danstahl1138@gmail.com>
2024-06-30 14:13:29 -0500

    cleanup

--- server/index.js
+++ server/index.js
@@ -1,51 +1,14 @@
 'use strict'
 const Fastify = require('fastify')
-//const http = require('http');
-//const httpProxy = require('@fastify/http-proxy')
-const { exec, execFile } = require('child_process');
-const { env: { /*PORT, IP,*/ USB_INDEX } } = require("process")
+const { execFile } = require('child_process');
+const { env: { USB_INDEX } } = require("process")
 
 const fastify = Fastify({
     logger: true
 })
 
-//const upstream = `http://${IP || "localhost"}:${PORT || 5380}`
 const usbIndex = USB_INDEX || 2
-/*fastify.register(httpProxy, {
-    upstream,
-    prefix: '/api', // optional
-});*/
 fastify.register(require('@fastify/websocket'))
-
-/*const options = {
-    hostname: IP || "localhost",
-    port: PORT || 5380,
-    path: '/devices/0',
-    //method: 'POST',
-};*/
-/*
-function httpGet(options) {
-    return new Promise((resolve, reject) => {
-        const req = http.request({
-            method: 'GET',
-            ...options,
-        }, res => {
-            const chunks = [];
-            res.on('data', data => chunks.push(data))
-            res.on('end', () => {
-                let resBody = Buffer.concat(chunks);
-                switch (res.headers['content-type']) {
-                    case 'application/json':
-                        resBody = JSON.parse(resBody);
-                        break;
-                }
-                resolve(resBody)
-            })
-        })
-        req.on('error', reject);
-        req.end();
-    })
-}*/
 
 //for me usbIndex is "2", run `uhubctl` to get a list of usb ports
 //uhubctl is assumed to be in your path, and index.js needs to run as root
@@ -136,18 +99,12 @@ fastify.register(async function (fastify) {
     fastify.get('/ws', { websocket: true }, (socket, req) => {
         const interval = setInterval(() => {
             Promise.all([
-                /*httpGet({...options }).then((data) => {
-                console.log(data.toString())
-                //console.log(volume)
-                //{ preset, mute, source, volume }
-                //socket.send(JSON.stringify({ preset, source, volume }))
-                }) */
                 minidspStatus(),
                 powerStatus(usbIndex)
             ]).then(([minidsp, power]) => {
                 const { preset, mute, source, volume } = minidsp
                 socket.send(JSON.stringify({ preset, source, volume, power }))
-            }).catch(console.err)
+            }).catch(console.log)
         }, 3000)
         socket.on("close", () => {
             console.log("closing...")
