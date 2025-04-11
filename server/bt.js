5c112638136eb2611d8d89f7e9f98ffb1d4e9b72
Daniel Stahl <danstahl1138@gmail.com>
2025-04-11 16:47:09 -0500

    another one

--- server/bt.js
+++ server/bt.js
@@ -54,8 +54,8 @@ const loopForDevice = async (adapter, deviceName) => {
     }
     return device
 }
-const GENERIC_ATTRIBUTE = "0000180f-0000-1000-8000-00805f9b34fb" //"00001801-0000-1000-8000-00805f9b34fb"
-const CUSTOM_ATTRIBUTE = "00002a19-0000-1000-8000-00805f9b34fb" //"00002a05-0000-1000-8000-00805f9b34fb"
+const GENERIC_ATTRIBUTE = "0000180a-0000-1000-8000-00805f9b34fb" //"00001801-0000-1000-8000-00805f9b34fb"
+const CUSTOM_ATTRIBUTE = "00002a23-0000-1000-8000-00805f9b34fb" //"00002a05-0000-1000-8000-00805f9b34fb"
 
 const tempGetCharacteristic = async (services) => {
     Object.entries(services).forEach(([key, value]) => {
