f6fe1f3c22002c4ab2c87ef0cb8854297d96c35b
Daniel Stahl <danstahl1138@gmail.com>
2025-04-11 07:03:56 -0500

    more logging

--- server/bt_discovery.js
+++ server/bt_discovery.js
@@ -4,9 +4,12 @@ const { createBluetooth } = require('node-ble')
 const { bluetooth } = createBluetooth()
 
 const doBt = async () => {
+    console.log("starting adapter")
     const adapter = await bluetooth.defaultAdapter()
+    console.log("starting discovery")
     if (! await adapter.isDiscovering())
         await adapter.startDiscovery()
+    console.log("starting device list")
     const devices = await adapter.devices()
     devices.forEach((deviceUuid) => {
         console.log(deviceUuid)
