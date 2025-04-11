294780634ca100ed98528dfa9c5e2aa18484b6c3
Daniel Stahl <danstahl1138@gmail.com>
2025-04-11 07:02:25 -0500

    actually get devices

--- server/bt_discovery.js
+++ server/bt_discovery.js
@@ -7,8 +7,9 @@ const doBt = async () => {
     const adapter = await bluetooth.defaultAdapter()
     if (! await adapter.isDiscovering())
         await adapter.startDiscovery()
-
-    btDevices.forEach((deviceUuid) => {
+    const devices = await adapter.devices()
+    devices.forEach((deviceUuid) => {
+        console.log(deviceUuid)
         adapter.waitDevice(deviceUuid).then(device => {
             Promise.all([device.getName(), device.getAlias()]).then(([name, alias]) => {
                 console.log("Device:", name, alias)
