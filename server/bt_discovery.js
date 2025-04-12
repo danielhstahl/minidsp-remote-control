44c6fff7c45c025ef9b1e3c09020b2a2c544a8d9
Daniel Stahl <danstahl1138@gmail.com>
2025-04-12 06:32:38 -0500

    Try raw hex

--- server/bt_discovery.js
+++ server/bt_discovery.js
@@ -27,7 +27,7 @@ const doHid = async () => {
     var devices = await HID.devicesAsync();
     console.log(devices)
 
-    var device = await HID.HIDAsync.open(2007, 0);
+    var device = await HID.HIDAsync.open(0x07d7, 0x0000);
     device.on("data", function (data) {
         console.log(data)
     });
