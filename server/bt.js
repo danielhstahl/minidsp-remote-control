f48f8a4315e34d6be332607de57bbf34452539f9
Daniel Stahl <danstahl1138@gmail.com>
2025-04-12 19:16:26 -0500

    cleaned

--- server/bt.js
+++ server/bt.js
@@ -2,9 +2,11 @@
 
 const { execFile } = require('child_process');
 const { createBluetooth } = require('node-ble')
-const { bluetooth, destroy } = createBluetooth()
+const { bluetooth } = createBluetooth()
 const HID = require('node-hid');
-
+const TIMEOUT_MS = 1000
+const HID_DEVICE_ID = 2007
+const HID_VENDOR_ID = 0
 function setMinidspVol(gain) {
     return new Promise((res, rej) => execFile(`minidsp`, ['gain', '--relative', '--', gain], (err, stdout, stderr) => {
         if (err) {
@@ -16,8 +18,7 @@ function setMinidspVol(gain) {
     }))
 }
 
-
-const findDevice = async (adapter, deviceName) => {
+const _findDevice = async (adapter, deviceName) => {
     const devices = await adapter.devices()
     const results = await Promise.all(devices.map(deviceUuid => {
         return adapter.waitDevice(deviceUuid).then(device => {
@@ -29,75 +30,66 @@ const findDevice = async (adapter, deviceName) => {
     return results.find(result => result.isDevice)?.device
 }
 
-const secondTimeout = async () => await new Promise((res) => setTimeout(res, 1000))
+const secondTimeout = async () => await new Promise((res) => setTimeout(res, TIMEOUT_MS))
 const loopForDevice = async (adapter, deviceName) => {
     let device = null
     if (!await adapter.isDiscovering()) {
         await adapter.startDiscovery()
     }
-    device = await findDevice(adapter, deviceName)
+    device = await _findDevice(adapter, deviceName)
     while (!device) {
         await secondTimeout()
-        device = await findDevice(adapter, deviceName)
+        device = await _findDevice(adapter, deviceName)
     }
     return device
 }
-
-const session = async (device) => {
-    console.log("device connecting")
+//will loop until connection is established; if VOL20 is turned off will loop indefinitely
+const loopForConnection = async (device) => {
     while (!await device.isConnected()) {
         try {
+            //I wish there was a way to check that device was available without waiting for it to error...
             await device.connect()
         }
         catch (exception) {
-            console.log("device not connected, continuing to try")
+            //it is expected to get here if device has been disconnected
         }
         await secondTimeout()
     }
-    console.log("device connected!")
-    const hid = await HID.HIDAsync.open(2007, 0);
+}
+const hidSession = async () => {
+    const hid = await HID.HIDAsync.open(HID_DEVICE_ID, HID_VENDOR_ID);
     hid.on("data", function (data) {
         const [dataType] = data
         if (dataType === 1) { ///volume down
-            console.log("VOL DOWN")
             setMinidspVol(-1)
         }
         if (dataType === 2) { ///volume up
-            console.log("VOL UP")
             setMinidspVol(1)
         }
     });
-
     return new Promise((res) => {
         hid.on("error", function (err) {
-            //this runs when bluetooth device is turned off
+            //error is invoked when bluetooth device is turned off
             console.log(err)
-            device.disconnect().catch(console.log).then(() => {
-                //destroy()
-                res("session ended")
-            })
+            hid.close()
+            res("session ended")
         })
     })
 }
 
 const doBt = async () => {
     const adapter = await bluetooth.defaultAdapter()
-    while (true) {
+    while (true) { //one loop per "session" (VOL20 on and active/connected)
         console.log("getting device")
         const device = await loopForDevice(adapter, "VOL20")
         console.log("device obtained")
-        await session(device) //waits until session is finished (disconnected)
+        console.log("device connecting...")
+        await loopForConnection(device)
+        console.log("device connected!")
+        await hidSession(device) //waits until session is finished (disconnected)
+        await device.disconnect()
         console.log("session ended")
     }
-    //const gattServer = await deviceInstance.gatt()
-    //const service2 = await gattServer.getPrimaryService(deviceUuid)
-    //const characteristics = await gattServer.characteristics()
-    //console.log(characteristics)
-    /*const characteristic2 = await service2.getCharacteristic('uuid')
-    characteristic2.on('valuechanged', buffer => {
-        console.log(buffer)
-    })
-    await characteristic2.startNotifications()*/
 }
 
 doBt()
--- server/bt_discovery.js
+++ server/bt_discovery.js
@@ -16,58 +16,8 @@ const doBt = async () => {
         const device = await adapter.waitDevice(deviceUuid)
         const alias = await device.getAlias()
         console.log("Device alias:", alias)
-        await device.connect()
         return true
     })).then(() => console.log("got them all"))
 }
 
 doBt()
-/*
-function setMinidspVol(gain) {
-    return new Promise((res, rej) => execFile(`minidsp`, ['gain', '--relative', '--', gain], (err, stdout, stderr) => {
-        if (err) {
-            rej(err)
-        }
-        else {
-            res()
-        }
-    }))
-}
-*/
-
-/*
-const HID = require('node-hid');
-const doHid = async () => {
-    while (true) {
-        try {
-            const hid = await HID.HIDAsync.open(2007, 0);
-
-            hid.on("data", function (data) {
-                const [dataType] = data
-                if (dataType === 1) { ///volume down
-                    console.log("VOL DOWN")
-                    setMinidspVol(-1)
-                }
-                if (dataType === 2) { ///volume up
-                    console.log("VOL UP")
-                    setMinidspVol(1)
-                }
-            });
-            await new Promise((res) => {
-                hid.on("error", function (err) {
-                    //this runs when bluetooth device is turned off
-                    res("session ended")
-                })
-            })
-        }
-        catch (exception) {
-            console.log("No device")
-        }
-        await new Promise((res) => setTimeout(res, 1000))
-    }
-
-}
-
-
-doHid()
-*/
