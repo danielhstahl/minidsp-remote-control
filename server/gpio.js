fabe9decd6edb919418c29db0b6d5c603755c191
Daniel Stahl <danstahl1138@gmail.com>
2025-05-10 06:46:25 -0500

    custom onoff

--- server/gpio.js
+++ server/gpio.js
@@ -1,58 +1,31 @@
-//gpio:
-//01 is 26 -> 7
-//02 is 20 -> 
-//03 is 21 -> 9
-const { writeFile, readFile } = require('fs')
+"use strict";
+const GPIO = require("./onoff").Gpio;
 
-const PATH = '/sys/class/gpio'
-const DIR_IN = 'in'
-const DIR_OUT = 'out'
-const OFF = '0'
-const ON = '1'
-
-const promisify = file_fn => (v1, v2) => new Promise((res, rej) => {
-    file_fn(v1, v2, (err, v) => {
-        if (err) {
-            rej(err)
-        }
-        else {
-            res(v)
-        }
-    })
-})
-
-const writeFilePs = promisify(writeFile)
-const readFilePs = promisify(readFile)
+//const DIR_IN = "in";
+const DIR_OUT = "out";
+const OFF = 0;
+const ON = 1;
 
 const openPin = (pin) => {
-    return writeFilePs(PATH + '/export', pin)
-}
+  return new GPIO(pin, DIR_OUT);
+};
+const toggle = (gpio, type) => {
+  return gpio.write(type);
+};
+const turnOn = (gpio) => {
+  return toggle(gpio, ON);
+};
 
-const closePin = (pin) => {
-    return writeFilePs(PATH + '/unexport', pin)
-}
+const turnOff = (gpio) => {
+  return toggle(gpio, OFF);
+};
 
-const toggle = (pin, type) => {
-    return writeFilePs(PATH + `/gpio${pin}/direction`, DIR_OUT).then(() => {
-        return writeFilePs(PATH + `/gpio${pin}/value`, type)
-    })
-}
-const turnOn = (pin) => {
-    return toggle(pin, ON)
-}
-
-const turnOff = (pin) => {
-    return toggle(pin, OFF)
-}
-
-const getStatus = (pin) => {
-    return writeFilePs(PATH + `/gpio${pin}/direction`, DIR_IN).then(() => {
-        return readFilePs(PATH + `/gpio${pin}/value`, 'utf-8')
-    })
-}
+const getStatus = (gpio) => {
+  return gpio.read();
+};
 module.exports = {
-    turnOn,
-    turnOff,
-    getStatus,
-    openPin
-}
+  openPin,
+  turnOn,
+  turnOff,
+  getStatus,
+};
--- server/index.js
+++ server/index.js
@@ -1,270 +1,188 @@
-'use strict'
-const Fastify = require('fastify')
-const { execFile } = require('child_process');
-const { env: { USB_INDEX } } = require("process")
-const { openPin, turnOn, turnOff, getStatus } = require('./gpio')
-const path = require("path")
-//const gpio = require('rpi-gpio').promise
+"use strict";
+const Fastify = require("fastify");
+const { execFile } = require("child_process");
+const { turnOn, turnOff, getStatus } = require("./gpio");
+const path = require("path");
 const fastify = Fastify({
-    logger: true
-})
+  logger: true,
+});
 
-const usbIndex = USB_INDEX || 2
+//cat /sys/kernel/debug/gpio
+const RELAY_1 = 538;
 
-//gpio:
-//01 is 26
-//02 is 20
-//03 is 21
-//const RELAY_1 = 26
-const RELAY_1 = "538"
-//gpio.setup(RELAY_1, gpio.DIR_OUT);
-
-//for me usbIndex is "2", run `uhubctl` to get a list of usb ports
-//uhubctl is assumed to be in your path, and index.js needs to run as root
-//see https://www.byfarthersteps.com/6802/
-function powerOff() {
-    return turnOff(RELAY_1)
-    /*return gpio.setup(RELAY_1, gpio.DIR_OUT)
-        .then(() => {
-            return gpio.write(RELAY_1, false)
-        })
-        .catch((err) => {
-            console.log('Error: ', err.toString())
-        })*/
-    /*return new Promise((res, rej) => gpio.write(RELAY_1, false, (err) => {
-        if (err) rej(err)
-        console.log('Written false to pin');
-        res()
-    }))*/
-
-    /*return new Promise((res, rej) => execFile(
-        `uhubctl`,
-        ['-l', '1-1', '-p', usbIndex, '-a', 'off'],
-        (err, stdout, stderr) => {
-            if (err) {
-                rej(err)
-            }
-            else {
-                res(stdout)
-            }
-        }))*/
+function powerOff(gpio) {
+  return turnOff(gpio);
 }
 
-function powerOn() {
-    return turnOn(RELAY_1) /*gpio.setup(RELAY_1, gpio.DIR_OUT)
-        .then(() => {
-            return gpio.write(RELAY_1, true)
-        })
-        .catch((err) => {
-            console.log('Error: ', err.toString())
-        })*/
-    /*return new Promise((res, rej) => gpio.write(RELAY_1, true, (err) => {
-        if (err) rej(err)
-        console.log('Written True to pin');
-        res()
-    }))*/
-
-    /*return new Promise((res, rej) => execFile(
-        `uhubctl`,
-        ['-l', '1-1', '-p', usbIndex, '-a', 'off'],
-        (err, stdout, stderr) => {
-            if (err) {
-                rej(err)
-            }
-            else {
-                res(stdout)
-            }
-        }))*/
+function powerOn(gpio) {
+  return turnOn(gpio);
 }
 
-//for me usbIndex is "2", run `uhubctl` to get a list of usb ports
-//uhubctl is assumed to be in your path, and index.js needs to run as root
-//see https://www.byfarthersteps.com/6802/
-/*function powerOn(usbIndex) {
-    return new Promise((res, rej) => execFile(
-        `uhubctl`,
-        ['-l', '1-1', '-p', usbIndex, '-a', 'on'],
-        (err, stdout, stderr) => {
-            if (err) {
-                rej(err)
-            }
-            else {
-                res(stdout)
-            }
-        }))
-}*/
-
-//for me usbIndex is "2", run `uhubctl` to get a list of usb ports
-//uhubctl is assumed to be in your path, and index.js needs to run as root
-//see https://www.byfarthersteps.com/6802/
-async function powerStatus() {
-    const result = await getStatus(RELAY_1)
-    return result == "0" ? "off" : "on"
-    /*const setup = await gpio.setup(RELAY_1, gpio.DIR_IN)
-    console.log("past setup")
-    const result = await gpio.read(RELAY_1)
-    console.log("power status")
-    console.log(result)*/
-    /*.then(async () => {
-        console.log("past power status")
-
-        return result ? "on" : "off"
-    })
-    .catch((err) => {
-        console.log('Error: ', err.toString())
-    })*/
-    /*return new Promise((res, rej) => gpio.read(RELAY_1, (err, result) => {
-        if (err) rej(err)
-        console.log("power status")
-        console.log(result)
-        res(result ? "on" : "off")
-    }))*/
-    /*return new Promise((res, rej) => execFile(
-        `uhubctl`,
-        ['-l', '1-1', '-p', usbIndex],
-        (err, stdout, stderr) => {
-            if (err) {
-                rej(err)
-            }
-            else {
-                res(stdout.search("off") === -1 ? "on" : "off")
-            }
-        }))*/
+async function powerStatus(gpio) {
+  const result = await getStatus(gpio);
+  return result === 0 ? "off" : "on";
 }
 
 function minidspStatus() {
-    return new Promise((res, rej) => execFile(`minidsp`, ['-o', 'json'], (err, stdout, stderr) => {
-        if (err) {
-            rej(err)
-        }
-        else {
-            res(JSON.parse(stdout).master)
-        }
-    }))
+  return new Promise((res, rej) =>
+    execFile(`minidsp`, ["-o", "json"], (err, stdout, stderr) => {
+      if (err) {
+        rej(err);
+      } else {
+        res(JSON.parse(stdout).master);
+      }
+    }),
+  );
 }
 
 function setMinidspVol(gain) {
-    return new Promise((res, rej) => execFile(`minidsp`, ['gain', '--', gain], (err, stdout, stderr) => {
-        if (err) {
-            rej(err)
-        }
-        else {
-            res()
-        }
-    }))
+  return new Promise((res, rej) =>
+    execFile(`minidsp`, ["gain", "--", gain], (err, stdout, stderr) => {
+      if (err) {
+        rej(err);
+      } else {
+        res();
+      }
+    }),
+  );
 }
 
 function incrementMinidspVol(gain) {
-    return new Promise((res, rej) => execFile(`minidsp`, ['gain', '--relative', '--', gain], (err, stdout, stderr) => {
+  return new Promise((res, rej) =>
+    execFile(
+      `minidsp`,
+      ["gain", "--relative", "--", gain],
+      (err, stdout, stderr) => {
         if (err) {
-            rej(err)
+          rej(err);
+        } else {
+          res();
         }
-        else {
-            res()
-        }
-    }))
+      },
+    ),
+  );
 }
 
-function setMinidspPreset(preset) { //0 indexed
-    return new Promise((res, rej) => execFile(`minidsp`, ['config', preset], (err, stdout, stderr) => {
-        if (err) {
-            rej(err)
-        }
-        else {
-            res()
-        }
-    }))
+function setMinidspPreset(preset) {
+  //0 indexed
+  return new Promise((res, rej) =>
+    execFile(`minidsp`, ["config", preset], (err, stdout, stderr) => {
+      if (err) {
+        rej(err);
+      } else {
+        res();
+      }
+    }),
+  );
 }
 
 function setMinidspInput(source) {
-    // note that the source output from minidspStatus has first letter capitalized, 
-    // but setting the source requires lowercase
-    // see https://minidsp-rs.pages.dev/cli/master/source
-    return new Promise((res, rej) => execFile(`minidsp`, ['source', source.toLowerCase()], (err, stdout, stderr) => {
+  // note that the source output from minidspStatus has first letter capitalized,
+  // but setting the source requires lowercase
+  // see https://minidsp-rs.pages.dev/cli/master/source
+  return new Promise((res, rej) =>
+    execFile(
+      `minidsp`,
+      ["source", source.toLowerCase()],
+      (err, stdout, stderr) => {
         if (err) {
-            rej(err)
+          rej(err);
+        } else {
+          res();
         }
-        else {
-            res()
-        }
-    }))
+      },
+    ),
+  );
 }
-const VOLUME_INCREMENT = 0.5
+const VOLUME_INCREMENT = 0.5;
 fastify.register(async function (fastify) {
-    await openPin(RELAY_1).catch(console.log)
-    fastify.get('/status', (req, reply) => {
-        Promise.all([
-            minidspStatus(),
-            powerStatus()
-        ]).then(([minidsp, power]) => {
-            const { preset, mute, source, volume } = minidsp
-            reply.send({ preset, source, volume, power })
-        }).catch((e) => {
-            reply.send({ success: false, message: e })
-        })
-    })
-    fastify.post("/volume/up", (req, reply) => {
-        incrementMinidspVol(VOLUME_INCREMENT).then(() => {
-            reply.send({ success: true })
-        }).catch((e) => {
-            reply.send({ success: false, message: e })
-        })
-    })
-    fastify.post("/volume/down", (req, reply) => {
-        incrementMinidspVol(-VOLUME_INCREMENT).then(() => {
-            reply.send({ success: true })
-        }).catch((e) => {
-            reply.send({ success: false, message: e })
-        })
-    })
-    fastify.post("/volume/:volume", (req, reply) => {
-        const { volume } = req.params
-        setMinidspVol(volume).then(() => {
-            reply.send({ success: true })
-        }).catch((e) => {
-            reply.send({ success: false, message: e })
-        })
-    })
-    fastify.post("/preset/:preset", (req, reply) => {
-        const { preset } = req.params
-        setMinidspPreset(preset).then(() => {
-            reply.send({ success: true })
-        }).catch((e) => {
-            reply.send({ success: false, message: e })
-        })
-    })
-    fastify.post("/source/:source", (req, reply) => {
-        const { source } = req.params
-        setMinidspInput(source).then(() => {
-            reply.send({ success: true })
-        }).catch((e) => {
-            reply.send({ success: false, message: e })
-        })
-    })
-    fastify.post("/power/on", (req, reply) => {
-        powerOn().then(() => {
-            reply.send({ success: true })
-        }).catch((e) => {
-            reply.send({ success: false, message: e })
-        })
-    })
-    fastify.post("/power/off", (req, reply) => {
-        powerOff().then(() => {
-            reply.send({ success: true })
-        }).catch((e) => {
-            reply.send({ success: false, message: e })
-        })
-    })
-})
+  const gpio = openPin(RELAY_1);
+  fastify.get("/status", (req, reply) => {
+    Promise.all([minidspStatus(), powerStatus(gpio)])
+      .then(([minidsp, power]) => {
+        const { preset, mute, source, volume } = minidsp;
+        reply.send({ preset, source, volume, power });
+      })
+      .catch((e) => {
+        reply.send({ success: false, message: e });
+      });
+  });
+  fastify.post("/volume/up", (req, reply) => {
+    incrementMinidspVol(VOLUME_INCREMENT)
+      .then(() => {
+        reply.send({ success: true });
+      })
+      .catch((e) => {
+        reply.send({ success: false, message: e });
+      });
+  });
+  fastify.post("/volume/down", (req, reply) => {
+    incrementMinidspVol(-VOLUME_INCREMENT)
+      .then(() => {
+        reply.send({ success: true });
+      })
+      .catch((e) => {
+        reply.send({ success: false, message: e });
+      });
+  });
+  fastify.post("/volume/:volume", (req, reply) => {
+    const { volume } = req.params;
+    setMinidspVol(volume)
+      .then(() => {
+        reply.send({ success: true });
+      })
+      .catch((e) => {
+        reply.send({ success: false, message: e });
+      });
+  });
+  fastify.post("/preset/:preset", (req, reply) => {
+    const { preset } = req.params;
+    setMinidspPreset(preset)
+      .then(() => {
+        reply.send({ success: true });
+      })
+      .catch((e) => {
+        reply.send({ success: false, message: e });
+      });
+  });
+  fastify.post("/source/:source", (req, reply) => {
+    const { source } = req.params;
+    setMinidspInput(source)
+      .then(() => {
+        reply.send({ success: true });
+      })
+      .catch((e) => {
+        reply.send({ success: false, message: e });
+      });
+  });
+  fastify.post("/power/on", (req, reply) => {
+    powerOn(gpio)
+      .then(() => {
+        reply.send({ success: true });
+      })
+      .catch((e) => {
+        reply.send({ success: false, message: e });
+      });
+  });
+  fastify.post("/power/off", (req, reply) => {
+    powerOff(gpio)
+      .then(() => {
+        reply.send({ success: true });
+      })
+      .catch((e) => {
+        reply.send({ success: false, message: e });
+      });
+  });
+});
 
-fastify.register(require('@fastify/static'), {
-    root: path.join(__dirname, 'build'),
-    prefix: '/', // optional: default '/'
-})
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "build"),
+  prefix: "/", // optional: default '/'
+});
 
 // Run the server!
 try {
-    fastify.listen({ port: 4000, host: '0.0.0.0' })
+  fastify.listen({ port: 4000, host: "0.0.0.0" });
 } catch (err) {
-    fastify.log.error(err)
+  fastify.log.error(err);
 }
