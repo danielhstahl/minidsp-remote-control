99faf694bbe487d31f99372e86cc8c4d7d9e882e
Daniel Stahl <danstahl1138@gmail.com>
2026-07-19 18:12:20 -0500

    add pwa plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
   server: {
     proxy: {
       "/api": {
